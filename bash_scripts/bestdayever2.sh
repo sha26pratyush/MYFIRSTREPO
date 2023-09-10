a5521a66f3c7eeb79a45502680b1acdcd5dd4043
pratyush <pratyushsharma200526@gmail.com>
2023-09-11 00:45:07 +0530

    new basic attribute and variables containing .sh file added

---
+++ bash_scripts/bestdayever2.sh
@@ -0,0 +1,13 @@
+#$!/bin/bash
+
+name=$1
+compliment=$2
+
+echo "Good Morning $name!!"
+sleep 2 
+
+echo "You're looking good today $name!!"
+sleep 2
+
+echo "You have the best $compliment I've ever heard to, $name!!"
+sleep 2
