354617491d4ac1bd601308056dc1696a5c796bce
pratyush <pratyushsharma200526@gmail.com>
2023-09-10 23:52:02 +0530

    added variables and arguments to bestdayever.sh

--- bash_scripts/bestdayever.sh
+++ bash_scripts/bestdayever.sh
@@ -1,10 +1,19 @@
 #$!/bin/bash
-name="Bhoomi"
+
+name=$1
+compliment=$2
+
+user=$(whoami) 
+whereami=$(pwd)
+date=$(date) 
 
 echo "Good Morning $name!!"
 sleep 2 
+
 echo "You're looking good today $name!!"
 sleep 2
-echo "You have the best voice I've ever heard to, $name!!"
 
+echo "You have the best $compliment I've ever heard to, $name!!"
+sleep 2
 
+echo "You are currently logged in as $user and you are in the directory $whereami. Also today is: $date"
