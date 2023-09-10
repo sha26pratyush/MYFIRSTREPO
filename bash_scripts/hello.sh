ace571c64de5f70edcdeab01d8b76c97f2fb63d6
pratyush <pratyushsharma200526@gmail.com>
2023-09-10 22:46:47 +0530

    my first bash script

---
+++ bash_scripts/hello.sh
@@ -0,0 +1,20 @@
+#!/bin/bash 
+
+echo "HELLO BHOOMI!"
+
+sleep 3
+
+echo "Ohh! You Look so pretty as usual!"
+
+sleep 3 
+
+echo "Awww, Thanks a lot sweetheart!"
+
+sleep 3
+
+echo "Ohhh... Gotta go."
+
+sleep 3 
+
+echo "Okay bye! Have a nice day"
+
