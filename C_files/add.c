ffa449eae40e0042a40ac63a50fe68fb4a2aae46
pratyush <pratyushsharma200526@gmail.com>
2023-09-25 21:40:32 +0530

    program which adds two numbers taking input from the user

---
+++ C_files/add.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main () {
+
+int a, b, sum ;
+
+printf("Enter the first number\n");
+	scanf("%d" , &a);
+
+printf("Enter the second number\n");
+	scanf("%d" , &b);
+
+sum = a + b ;
+
+printf("Sum of the two numbers is %d \n", sum);
+return 0;
+}
