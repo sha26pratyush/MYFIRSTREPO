2bcf31425db9d2d1a412b24b066559d11edda82f
pratyush <pratyushsharma200526@gmail.com>
2023-09-25 22:03:35 +0530

    can now calculate subtraction, multiplication and division

--- C_files/add.c
+++ C_files/add.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main () {
 
-int a, b, sum ;
+int a, b, sum, sub, multi, div ;
 
 printf("Enter the first number\n");
 	scanf("%d" , &a);
@@ -10,7 +10,14 @@ printf("Enter the second number\n");
 	scanf("%d" , &b);
 
 sum = a + b ;
+sub = a - b ;
+multi = a*b ;
+div = a / b ; 
 
 printf("Sum of the two numbers is %d \n", sum);
+printf("Subtraction of the two numbers is %d \n", sub);
+printf("Multiplication of the two numbers is %d \n", multi);
+printf("Division of the two numbers is %d \n", div);
+
 return 0;
 }
