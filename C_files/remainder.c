4dea4f7deea38531db98722f99c9c1ad56dd340e
pratyush <pratyushsharma200526@gmail.com>
2023-09-26 23:58:42 +0530

    finding remainder by taking input from the user

---
+++ C_files/remainder.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main () {
+	int a, b, remainder ;
+	printf("Enter the first number:\n ") ;
+	scanf("%d" , a) ;
+	printf("Enter the second number:\n ") ;
+	scanf("%d" , b) ;
+
+	remain = a % b, remainder ;
+	return 0 ;
+}
