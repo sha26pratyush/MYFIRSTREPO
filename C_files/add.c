905ceb92134db32a9a3938c7cdf33536ddb7885f
pratyush <pratyushsharma200526@gmail.com>
2023-09-28 10:27:20 +0530

    add.c modified into a smaller file by printing the output in the same printf function

--- C_files/add.c
+++ C_files/add.c
@@ -14,10 +14,7 @@ sub = a - b ;
 multi = a*b ;
 div = a / b ; 
 
-printf("Sum of the two numbers is %d \n", sum);
-printf("Subtraction of the two numbers is %d \n", sub);
-printf("Multiplication of the two numbers is %d \n", multi);
-printf("Division of the two numbers is %d \n", div);
+printf("Sum of the two numbers is %d.\n Subtraction of the two numbers is %d.\n Multiplication of the two numbers is %d.\n Division of the two numbers is %d.\n", sum, sub, multi, div);
 
 return 0;
 }
