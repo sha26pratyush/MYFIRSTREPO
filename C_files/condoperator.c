42a8856cc4f7ddd4faa531f5ed9af914b43a2c58
pratyush <pratyushsharma200526@gmail.com>
2023-10-14 05:16:18 +0530

    my first C program using conditional operators

---
+++ C_files/condoperator.c
@@ -0,0 +1,12 @@
+/* My first program using conditional opeators */ 
+
+#include<stdio.h>
+int main (void)
+{
+int a ;
+printf("Enter the first number : \n");
+scanf("%d",&a);
+(a==0)?printf("a is equal to zero\n"):printf("a is not equal to zero\n");
+}
+
+
