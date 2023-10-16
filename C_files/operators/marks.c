679af30652a664f87b663d0bbe6caf680ada1217
pratyush <pratyushsharma200526@gmail.com>
2023-10-17 01:32:14 +0530

    added a function that the marks obtained in a subject cannot be more than 100 using logical OR operator and if it does then the program exits

--- C_files/operators/marks.c
+++ C_files/operators/marks.c
@@ -7,9 +7,9 @@ int main(void) {
 
     printf("Enter your first name: ");
     fgets(name, 15, stdin);
-    printf("Hello %s", name);
+    printf("You are %s\n", name);
 
-    printf("\nEnter the marks obtained in Physics: ");
+    printf("Enter the marks obtained in Physics: ");
     scanf("%d", &phy);
     printf("Enter the marks obtained in Chemistry: ");
     scanf("%d", &che);
@@ -20,20 +20,19 @@ int main(void) {
     printf("Enter the marks obtained in CSE: ");
     scanf("%d", &cse);
 
-    int res = (phy <= 100 && che <= 100 && math <= 100 && eng <= 100 && cse <= 100);
-
-    if (res == 0) {
-        printf("\n\nMarks in a subject cannot be more than 100.\nExiting program...\n");
+    // Check if any subject's marks are greater than 100
+    if (phy > 100 || che > 100 || math > 100 || eng > 100 || cse > 100) {
+        printf("\n\nError: Marks in a subject cannot be more than 100.\nExiting program...\n");
         exit(1);
     }
 
     float total = phy + che + math + eng + cse;
     float percentage = total / 500 * 100;
-    float required = 33; 
+    float required = 33;
 
     printf("The percentage obtained is %f\n", percentage);
-
-(percentage >= required) ? printf("PASS!\n") : printf("FAIL!\n");
+	
+    percentage >= required ? printf("PASS!\n") : printf("FAIL!\n");
 
 return 0;
 }
