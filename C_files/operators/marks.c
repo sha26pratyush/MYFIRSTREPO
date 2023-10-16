212c29c2f2fc9256a6b4022cd05086693b1b817e
pratyush <pratyushsharma200526@gmail.com>
2023-10-17 01:25:23 +0530

    added a function that the marks obtained in a subject cannot be more than 100 using logical AND operator and if it does then the program exits

--- C_files/operators/marks.c
+++ C_files/operators/marks.c
@@ -1,28 +1,40 @@
-#include<stdio.h>
-int main(void)
-{
-	int phy, che, math, bio, cse;
-	char name[15];
-	printf("Enter your first name: \n");
-	fgets(name, 15, stdin);
-	printf("You are %s \n", name);
-	printf("Enter the marks obtained in Physics: ");
-	scanf("%d", &phy);
-	printf("Enter the marks obtained in Chemistry: \n");
-	scanf("%d" , &che);
-	printf("Enter the marks obtained in Maths: \n");
-	scanf("%d" , &math);
-	printf("Enter the marks obtained in Biology: \n");
-	scanf("%d" , &bio);
-	printf("Enter the marks obtained in CSE: \n");
-	scanf("%d" , &cse);
-	
+#include <stdio.h>
+#include <stdlib.h>
 
-float total=phy+che+math+bio+cse,
-     percentage=total/500 * 100 ;
-float required=165 ; /*Out of 500. */
+int main(void) {
+    int phy, che, math, eng, cse;
+    char name[15];
 
-printf("The percentage obtained is %f \n" , percentage) ;
-	(total >= required)?printf("PASS!\n"):printf("FAIL!\n") ;
-} 
+    printf("Enter your first name: ");
+    fgets(name, 15, stdin);
+    printf("Hello %s", name);
+
+    printf("\nEnter the marks obtained in Physics: ");
+    scanf("%d", &phy);
+    printf("Enter the marks obtained in Chemistry: ");
+    scanf("%d", &che);
+    printf("Enter the marks obtained in Maths: ");
+    scanf("%d", &math);
+    printf("Enter the marks obtained in English: ");
+    scanf("%d", &eng);
+    printf("Enter the marks obtained in CSE: ");
+    scanf("%d", &cse);
+
+    int res = (phy <= 100 && che <= 100 && math <= 100 && eng <= 100 && cse <= 100);
+
+    if (res == 0) {
+        printf("\n\nMarks in a subject cannot be more than 100.\nExiting program...\n");
+        exit(1);
+    }
+
+    float total = phy + che + math + eng + cse;
+    float percentage = total / 500 * 100;
+    float required = 33; 
+
+    printf("The percentage obtained is %f\n", percentage);
+
+(percentage >= required) ? printf("PASS!\n") : printf("FAIL!\n");
+
+return 0;
+}
 
