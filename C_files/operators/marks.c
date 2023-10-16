279b1db54d197c02d89cd719fefed07eb18a45ed
pratyush <pratyushsharma200526@gmail.com>
2023-10-16 23:21:15 +0530

    user can now input their name

--- C_files/operators/marks.c
+++ C_files/operators/marks.c
@@ -1,9 +1,12 @@
 #include<stdio.h>
 int main(void)
 {
-	int phy, che, math, bio, cse ;
-
-	printf("Enter the marks obtained in Physics: \n");
+	int phy, che, math, bio, cse;
+	char name[15];
+	printf("Enter your first name: \n");
+	fgets(name, 15, stdin);
+	printf("You are %s \n", name);
+	printf("Enter the marks obtained in Physics: ");
 	scanf("%d", &phy);
 	printf("Enter the marks obtained in Chemistry: \n");
 	scanf("%d" , &che);
