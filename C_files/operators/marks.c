06424737fff2c18ea7550985f9335da064892b98
pratyush <pratyushsharma200526@gmail.com>
2023-10-18 21:42:25 +0530

    added a feature of printing 'you are fail in ' if marks obtained in a subject is less than 33

--- C_files/operators/marks.c
+++ C_files/operators/marks.c
@@ -20,6 +20,18 @@ int main(void) {
     printf("Enter the marks obtained in CSE: ");
     scanf("%d", &cse);
 
+    // Check if any subject's marks are less than 33
+	if (phy < 33){
+		printf("\nYou are failed in Physics.\n"); }
+	if (che < 33){
+		printf("\nYou are failed in Chemistry.\n"); }
+	if (math < 33){
+		printf("\nYou are failed in Maths.\n"); }
+	if (eng < 33){
+		printf("\nYou are failed in English.\n"); }
+	if (cse < 33){
+		printf("\nYou are failed in CSE.\n"); }
+
     // Check if any subject's marks are greater than 100
     if (phy > 100 || che > 100 || math > 100 || eng > 100 || cse > 100) {
         printf("\n\nError: Marks in a subject cannot be more than 100.\nExiting program...\n");
@@ -30,9 +42,9 @@ int main(void) {
     float percentage = total / 500 * 100;
     float required = 33;
 
-    printf("The percentage obtained is %f\n", percentage);
+    printf("\nYour overall percentage obtained is %f\n", percentage);
 	
-    percentage >= required ? printf("PASS!\n") : printf("FAIL!\n");
+    percentage >= required ? printf("\nPASS!\n") : printf("\nFAIL!\n");
 
 return 0;
 }
