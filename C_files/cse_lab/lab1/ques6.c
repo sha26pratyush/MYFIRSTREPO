9dab19465cbed99eb68e26cfa23c7544835e4d55
pratyush <pratyushsharma200526@gmail.com>
2023-10-29 23:58:33 +0530

    add comment

--- C_files/cse_lab/lab1/ques6.c
+++ C_files/cse_lab/lab1/ques6.c
@@ -1,6 +1,20 @@
-/* C program to evaluate equations of motion */
+#include <stdio.h>
 
-#include<stdio.h>
-int main(void)
+int main(void) {
+    float v, u, a, t;
 
+//program to calculate speed equations
+	printf("Enter the initial speed: ");
+    scanf("%f", &u);
+    printf("Enter the acceleration: ");
+    scanf("%f", &a);
+    printf("Enter the time traveled: ");
+    scanf("%f", &t);
+
+    v = u + a * t;
+    printf("The Final Speed (v) is %f.\n", v);
+
+//program to calculate s=ut+1/2at*t 
+    return 0;
+}
 
