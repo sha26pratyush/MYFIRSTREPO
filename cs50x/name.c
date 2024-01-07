3a8e4e9e03b1ae7945a1360de3a54a5c581f8ee8
bhoomi <bhoomiagrawal175@gmail.com>
2024-01-07 23:30:30 +0530

    name.c is modified

--- cs50x/name.c
+++ cs50x/name.c
@@ -1,8 +1,10 @@
-#include<cs50.h>
-#include<stdio.h>
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+
 int main() {
-  string name = get_string("What is your name? ");
-  printf("Hello, %s!\n", name);
-  return 0;
+	  string name = get_string("What is your name? ");
+	    printf("Hello, %s!\n", name);
+	      return 0;
 }
 
