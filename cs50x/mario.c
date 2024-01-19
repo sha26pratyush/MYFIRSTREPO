b4d92e553e4af43f24a0eca1f4b0bc11c85ada5a
pratyush <pratyushsharma200526@gmail.com>
2024-01-19 22:16:59 +0530

    program prints a horizontal row pattern

---
+++ cs50x/mario.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(void)
+{
+  int n;
+  int i, j;
+
+  printf("Enter the number of rows you want to print: ");
+  scanf("%d", &n);
+
+  for(i=0; i<n; i++)
+  {
+    printf("*");
+  }
+}
